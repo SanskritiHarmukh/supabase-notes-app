ac04d9477e397257dc5c9fd65e4ac24325911277
Sanskriti Harmukh <74777863+SanskritiHarmukh@users.noreply.github.com>
2025-09-26 14:21:31 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-import getConfig from "next/config";
+import { createClient } from "@supabase/supabase-js"
 
-const { publicRuntimeConfig } = getConfig();
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(
-  publicRuntimeConfig.supabaseUrl,
-  publicRuntimeConfig.supabaseAnonKey
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
