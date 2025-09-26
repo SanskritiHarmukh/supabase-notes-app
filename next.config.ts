c93ed049744498a894cec63ad1d28bd8b6bf5779
Sanskriti Harmukh <74777863+SanskritiHarmukh@users.noreply.github.com>
2025-09-26 16:31:03 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  publicRuntimeConfig: {
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
